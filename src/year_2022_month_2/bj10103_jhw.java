ed6def5a4b75fc0fbd301b864ca14d8e573924ab
cousim46 <wjdghldns24@naver.com>
2022-02-15 19:19:10 +0900

    bj10103_jhw

---
+++ src/year_2022_month_2/bj10103_jhw.java
@@ -0,0 +1,23 @@
+package year_2022_month_2;
+
+import java.util.Scanner;
+
+public class bj10103_jhw {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int changScore = 100;
+        int sangScore = 100;
+        for (int i = 0; i < N; i++) {
+            int chang = sc.nextInt();
+            int sang = sc.nextInt();
+            if (chang > sang) {
+                sangScore -= chang;
+            } else if (chang < sang) {
+                changScore -= sang;
+            }
+        }
+        System.out.println(changScore);
+        System.out.println(sangScore);
+    }
+}
