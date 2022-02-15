5d3093f1c0c100a2f51831f6c84127b1961c10e1
cousim46 <wjdghldns24@naver.com>
2022-02-15 15:13:45 +0900

    bj2845_jhw

---
+++ src/year_2022_month_2/bj2845_jhw.java
@@ -0,0 +1,18 @@
+package year_2022_month_2;
+
+import java.util.Scanner;
+
+public class bj2845_jhw {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int personCnt = sc.nextInt();
+        int width = sc.nextInt();
+        int partyPersonCnt = personCnt * width;
+        for (int i = 0; i < 5; i++) {
+            sb.append((sc.nextInt() - partyPersonCnt) + " ");
+        }
+        System.out.println(sb);
+
+    }
+}
