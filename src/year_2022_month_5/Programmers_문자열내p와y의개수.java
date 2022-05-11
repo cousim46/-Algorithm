f803153fa0466eabe368ece9fbdf8ee6f25cd588
cousim46 <wjdghldns24@naver.com>
2022-05-11 16:58:34 +0900

    Programmers_콜라츠추측

---
+++ src/year_2022_month_5/Programmers_문자열내p와y의개수.java
@@ -0,0 +1,21 @@
+package year_2022_month_5;
+
+public class Programmers_문자열내p와y의개수 {
+    boolean solution(String s) {
+        boolean answer = true;
+        int pCnt = 0;
+        int yCnt = 0;
+        for(int i = 0; i < s.length();i++) {
+            if(s.charAt(i) == 'p' || s.charAt(i) == 'P') {
+                pCnt++;
+            }else if(s.charAt(i) == 'y' || s.charAt(i) == 'Y') {
+                yCnt++;
+            }
+        }
+        if(pCnt != yCnt) {
+            answer = false;
+        }
+
+        return answer;
+    }
+}
