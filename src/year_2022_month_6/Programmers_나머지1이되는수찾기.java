d2e11c9fd1735971993aa243d4fe2c1d59b6edc0
cousim46 <wjdghldns24@naver.com>
2022-06-02 21:30:47 +0900

    Programmers_나머지1이되는수찾기

---
+++ src/year_2022_month_6/Programmers_나머지1이되는수찾기.java
@@ -0,0 +1,16 @@
+package year_2022_month_6;
+
+public class Programmers_나머지1이되는수찾기 {
+    public int solution(int n) {
+        int answer = 0;
+        int i = 1;
+        while (true) {
+            if (n % i == 1) {
+                answer = i;
+                break;
+            }
+            i++;
+        }
+        return answer;
+    }
+}
