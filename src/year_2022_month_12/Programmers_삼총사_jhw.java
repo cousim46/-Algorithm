c1e42a96975550292c1ce0d166707cd6527cf659
cousim46 <wjdghldns24@naver.com>
2022-12-02 09:16:53 +0900

    Programmers_삼총사_jhw

---
+++ src/year_2022_month_12/Programmers_삼총사_jhw.java
@@ -0,0 +1,24 @@
+package year_2022_month_12;
+
+public class Programmers_삼총사_jhw {
+    public static void main(String[] args) {
+        Programmers_삼총사_jhw p = new Programmers_삼총사_jhw();
+        int[] arr = {-2, 3, 0, 2, -5};
+        System.out.println(p.solution(arr));
+    }
+
+    public int solution(int[] number) {
+        int answer = 0;
+        for (int i = 0; i < number.length - 2; i++) {
+            for (int j = i + 1; j < number.length - 1; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    int sum = number[i] + number[j] + number[k];
+                    if (sum == 0) {
+                        answer++;
+                    }
+                }
+            }
+        }
+        return answer;
+    }
+}
